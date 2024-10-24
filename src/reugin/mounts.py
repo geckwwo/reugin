159e6042ae7981a33c9191f93c9f8592e65d05cc
geckwwo <unknown7official@gmail.com>
2024-10-24 22:49:39 +0600

    safer static content using pathlib tests

--- src/reugin/mounts.py
+++ src/reugin/mounts.py
@@ -4,12 +4,19 @@ from .server import RD_ERR_404
 import os
 import mimetypes
 import warnings
+from pathlib import Path
 
 def StaticContent(dir):
-    warnings.warn(RuntimeWarning("Static files are experimental and may cause security issues. Windows is known to be vulnerable right now - Unix-like systems should be protected though."))
+    # disable this warning for now - Path used should cover everything
+    #warnings.warn(RuntimeWarning("Static files are experimental and may cause security issues. Windows is known to be vulnerable right now - Unix-like systems should be protected though."))
+    base_dir = Path(dir).resolve()
+    base_path = str(base_dir)
     async def route(rq: Request, staticrel):
-        fp = f"{dir}{'/' if not dir.endswith('/') else ''}{staticrel}"
-        if not os.path.isfile(fp):
-            return HTMLResponse(404, RD_ERR_404)
-        return BinaryResponse(200, mimetypes.guess_type(fp)[0] or "application/octet-stream", open(fp, "rb").read())
+        file: Path = (base_dir / staticrel).resolve()
+        if not str(file).startswith(base_path):
+            # got outside somehow - reject
+            return HTMLResponse(404, RD_ERR_404.decode())
+        if not file.is_file():
+            return HTMLResponse(404, RD_ERR_404.decode())
+        return BinaryResponse(200, mimetypes.guess_type(str(file))[0] or "application/octet-stream", file.open("rb").read())
     return route
